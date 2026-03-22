a5f5319000450cafefbe05cfedebcc4ab23b37bd
Stuart krueger <stuartkrueger@Stuarts-MacBook-Pro.local>
2026-03-22 10:20:33 -0400

    SCV test and github action number 7

--- SSDFlask/visualization_scripts/tests/test_app.py
+++ SSDFlask/visualization_scripts/tests/test_app.py
@@ -4,15 +4,13 @@ from app import app
 def test_001_map_renders(dash_duo):
    dash_duo.start_server(app)
 
-   dash_duo.wait_for_element("#state-choropleth", timeout=10)
-
-   assert dash_duo.find_element("#state-choropleth").is_displayed()
+   dash_duo.wait_for_element("#state-choropleth", timeout=15)
 
 def test_002_dropdown_functional(dash_duo):
    dash_duo.start_server(app)
 
    dash_duo.wait_for_element("#decision-dropdown", timeout=10)
-
+    
    dropdown = dash_duo.find_element("#decision-dropdown")
    assert dropdown.is_displayed()
 
