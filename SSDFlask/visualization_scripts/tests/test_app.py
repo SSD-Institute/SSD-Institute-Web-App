68d34ab1fa7e2bc2f9acdd1e37c8174c2449b63b
Stuart krueger <stuartkrueger@Stuarts-MacBook-Pro.local>
2026-03-22 09:56:42 -0400

    SCV test and github action

---
+++ SSDFlask/visualization_scripts/tests/test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app
+
+def test_001_map_renders(dash_duo):
+   dash_duo.start_server(app)
+
+   dash_duo.wait_for_element("#state-choropleth", timeout=10)
+
+   assert "Supreme Court" in dash_duo.find_element("#state-choropleth").text
+
+   dash_duo.percy_snapshot("initial_map_load")
+
+def test_002_dropdown_functional(dash_duo):
+   dash_duo.start_server(app)
+
+   dropdown = dash_duo.find_element("#decision-dropdown")
+   dropdown.click()
+
+   assert dash_duo.get_logs() == [], "Browser console should be clean."
